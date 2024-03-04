c4e58d9ca05ef0537e3012612e482c906e66e561
mdalmamunit427 <mdalmamunit427@gmail.com>
2024-03-04 21:02:50 +0600

    client starter files

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.jsx';
-import AuthProvider from './utilities/providers/AuthProvider';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import 'react-toastify/dist/ReactToastify.css';
 import Aos from 'aos';
 import { QueryClient, QueryClientProvider, } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello, Developers!</div>}
+]);
+
 Aos.init();
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
-    </AuthProvider>
+    
 )
